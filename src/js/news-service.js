208b0df4c3b550ca7871da0db69263fbd7cd594d
samod mukadam <samodmukadam2005@gmail.com>
2022-10-31 15:44:58 -0700

    Add js

--- src/js/news-service.js
+++ src/js/news-service.js
@@ -2,7 +2,7 @@ export default class MarsConditions {
     static getConditions() {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        const url = `https://api.nasa.gov/planetary/apod?api_key=KqdZ3rPg5aKmZaGrahQYL7kZrUMh24s2G9R67r2x`;
+        const url = `https://api.maas2.apollorion.com`;
         request.onload = function() {
           if (this.status === 200) {
             resolve(request.response);
